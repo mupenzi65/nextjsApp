831da8a19da18a749da40ec5658192f04a54061a
mupenzi65 <mupenziabdul34@gmail.com>
2024-01-25 11:09:26 +0200

    adets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/ui/global.css';
-import { Inter } from 'next/font/google';
+
  
-export const inter = Inter({ subsets: ['latin'] });
+
 export default function RootLayout({
   children,
 }: {
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className='antialiased'>{children}</body>
     </html>
   );
 }
